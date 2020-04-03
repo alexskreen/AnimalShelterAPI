8b945cc6960226475a27dc37d8b1b7751435c1bb
Alex Skreen <skreenalex@gmail.com>
2020-04-03 13:06:08 -0700

    add full CRUD to API and test JWT token. Both succeeded

---
+++ AnimalShelterAPI/Migrations/20200403200135_SeedData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelterAPI.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cats",
+                columns: new[] { "CatId", "Age", "Breed", "Name" },
+                values: new object[,]
+                {
+                    { 1, 9, "Polydactyl Turkish Angora", "Stella" },
+                    { 2, 5, "Tuxedo", "Charlie" },
+                    { 3, 7, "Tabby", "Tina" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Dogs",
+                columns: new[] { "DogId", "Age", "Breed", "Name" },
+                values: new object[,]
+                {
+                    { 1, 15, "Golden Retriever", "Casey" },
+                    { 2, 10, "Flat Coat Retriever", "Brian" },
+                    { 3, 7, "Golden Retriever", "Frank" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cats",
+                keyColumn: "CatId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Dogs",
+                keyColumn: "DogId",
+                keyValue: 3);
+        }
+    }
+}
