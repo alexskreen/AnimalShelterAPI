c9a7f63e307511da8b965bce8489fcfbb225d42b
Alex Skreen <skreenalex@gmail.com>
2020-04-03 12:55:42 -0700

    add seeding

--- AnimalShelterAPI/Models/AnimalShelterAPIContext.cs
+++ AnimalShelterAPI/Models/AnimalShelterAPIContext.cs
@@ -8,7 +8,21 @@ namespace AnimalShelterAPI.Models
     public DbSet<Cat> Cats { get; set; }
 
     public AnimalShelterAPIContext(DbContextOptions<AnimalShelterAPIContext> options) : base(options) { }
-
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Dog>()
+      .HasData(
+        new Dog { DogId = 1, Name = "Casey", Breed = "Golden Retriever", Age = 15 },
+        new Dog { DogId = 2, Name = "Brian", Breed = "Flat Coat Retriever", Age = 10 },
+        new Dog { DogId = 3, Name = "Frank", Breed = "Golden Retriever", Age = 7 }
+      );
+      builder.Entity<Cat>()
+      .HasData(
+        new Cat { CatId = 1, Name = "Stella", Breed = "Polydactyl Turkish Angora", Age = 9 },
+        new Cat { CatId = 2, Name = "Charlie", Breed = "Tuxedo", Age = 5 },
+        new Cat { CatId = 3, Name = "Tina", Breed = "Tabby", Age = 7 }
+      );
+    }
   }
 
 }
